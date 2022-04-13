4c696b55f026b613fadf77d8743d3882b7c14274
49EHyeon42 <49EHyeon42@gmail.com>
2022-04-13 22:11:03 +0900

    Add 백준 11단계 정렬 6-11650-좌표 정렬하기

---
+++ baekjoon/단계별로 풀어보기/11-정렬/6-11650-좌표 정렬하기/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] array = new int[N][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array, (a, b) -> {
+            if (a[0] == b[0]) { // If the values of Y are the same
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(array[i][0] + " " + array[i][1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
