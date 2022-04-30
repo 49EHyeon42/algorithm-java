fa29b01b070fa07a3ec3fc635df62aa95075c826
49EHyeon42 <49EHyeon42@gmail.com>
2022-04-30 19:10:06 +0900

    Add 백준 16단계 누적 합 5-11660-구간 합 구하기 5

---
+++ baekjoon/단계별로 풀어보기/16-누적 합/5-11660-구간 합 구하기 5/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] array = new int[N + 1][N + 1];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                array[i + 1][j + 1] = array[i][j + 1] + array[i + 1][j] - array[i][j]
+                    + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+
+            int sum = array[y2][x2] - array[y1 - 1][x2] - array[y2][x1 - 1] + array[y1 - 1][x1 - 1];
+
+            sb.append(sum).append('\n');
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+}
