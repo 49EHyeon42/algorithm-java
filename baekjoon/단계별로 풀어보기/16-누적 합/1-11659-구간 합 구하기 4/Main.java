88d6e6eb1bb20376ae9b8c4117b44e4e5fa0cedc
49EHyeon42 <49EHyeon42@gmail.com>
2022-04-30 11:31:50 +0900

    Add 백준 16단계 누적 합 1-11659-구간 합 구하기 4

---
+++ baekjoon/단계별로 풀어보기/16-누적 합/1-11659-구간 합 구하기 4/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i < N + 1; i++) {
+            array[i] = array[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int last = Integer.parseInt(st.nextToken());
+            int top = Integer.parseInt(st.nextToken());
+
+            sb.append(array[top] - array[last - 1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+}
