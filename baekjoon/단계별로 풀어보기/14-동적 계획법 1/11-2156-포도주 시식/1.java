41884f7cb4217b822e0407b3a3a9e8916fe761c9
49EHyeon42 <49EHyeon42@gmail.com>
2022-05-30 19:20:13 +0900

    Add 백준 14단계 동적 계획법 1 11-2156-포도주 시식 1번 풀이

---
+++ baekjoon/단계별로 풀어보기/14-동적 계획법 1/11-2156-포도주 시식/1.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        if (N == 1) {
+            answer = Integer.parseInt(br.readLine());
+        } else if (N == 2) {
+            answer = Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine());
+        } else {
+            int[] array = new int[N + 1];
+            int[] dpArray = new int[N + 1];
+
+            for (int i = 1; i <= N; i++) {
+                array[i] = Integer.parseInt(br.readLine());
+            }
+
+            dpArray[1] = array[1];
+            dpArray[2] = array[1] + array[2];
+            for (int i = 3; i <= N; i++) {
+                dpArray[i] = Integer.max(
+                    Integer.max(dpArray[i - 3] + array[i - 1] + array[i],
+                        dpArray[i - 2] + array[i]), dpArray[i - 1]);
+            }
+
+            answer = dpArray[N];
+        }
+
+        bw.write(Integer.toString(answer));
+
+        bw.flush();
+        bw.close();
+    }
+}
