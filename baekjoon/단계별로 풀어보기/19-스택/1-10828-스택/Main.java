c2a13d10b191053e7479add2868e71e9e67ec1f5
49EHyeon42 <49EHyeon42@gmail.com>
2022-06-03 09:42:47 +0900

    Add 백준 19 단계 스택 1-10828-스택

---
+++ baekjoon/단계별로 풀어보기/19-스택/1-10828-스택/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            Integer write = null;
+            switch (st.nextToken()) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    write = (stack.empty()) ? -1 : stack.pop();
+                    break;
+                case "size":
+                    write = stack.size();
+                    break;
+                case "empty":
+                    write = (stack.empty()) ? 1 : 0;
+                    break;
+                case "top":
+                    write = (stack.empty()) ? -1 : stack.peek();
+                    break;
+            }
+
+            if (write != null) {
+                sb.append(write).append('\n');
+            }
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+}
