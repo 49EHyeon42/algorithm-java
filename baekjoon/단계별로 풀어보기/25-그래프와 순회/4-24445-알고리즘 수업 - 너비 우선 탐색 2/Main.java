7e2f456e8506ea4f463e75096dbdb1f00aa2577f
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-05 12:12:08 +0900

    Add 백준 25단계 그래프와 순회 4-24445-알고리즘 수업 - 너비 우선 탐색 2

---
+++ baekjoon/단계별로 풀어보기/25-그래프와 순회/4-24445-알고리즘 수업 - 너비 우선 탐색 2/Main.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int count = 1;
+    private static final ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    private static int[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int vertexCount = Integer.parseInt(st.nextToken());
+        int edgeCount = Integer.parseInt(st.nextToken());
+        int startVertex = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < vertexCount + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        isVisited = new int[vertexCount + 1];
+
+        for (int i = 0; i < edgeCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i < graph.size(); i++) {
+            graph.get(i).sort(Comparator.reverseOrder());
+        }
+
+        bfs(startVertex);
+
+        for (int i = 1; i < graph.size(); i++) {
+            sb.append(isVisited[i]).append('\n');
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bfs(int startVertex) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(startVertex);
+        isVisited[startVertex] = count++;
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+
+            for (int nextVertex : graph.get(currentVertex)) {
+                if (isVisited[nextVertex] <= 0) {
+                    isVisited[nextVertex] = count++;
+                    queue.offer(nextVertex);
+                }
+            }
+        }
+    }
+}
