f0593c0473273846e3ede2dd0bbd12a4ff980b22
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-05 19:57:30 +0900

    Add 백준 25단계 그래프와 순회 1-24479-알고리즘 수업 - 깊이 우선 탐색 1 3번 풀이

---
+++ baekjoon/단계별로 풀어보기/25-그래프와 순회/1-24479-알고리즘 수업 - 깊이 우선 탐색 1/3.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int count = 1;
+    private static final ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    private static int[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int vertexCount = Integer.parseInt(st.nextToken());
+        int edgeCount = Integer.parseInt(st.nextToken());
+        int startVertex = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < vertexCount + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        isVisited = new int[vertexCount + 1];
+
+        for (int i = 0; i < edgeCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i < graph.size(); i++) {
+            graph.get(i).sort(Comparator.reverseOrder());
+        }
+
+        dfs(startVertex);
+
+        for (int i = 1; i < graph.size(); i++) {
+            sb.append(isVisited[i]).append('\n');
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int startVertex) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(startVertex);
+
+        while (!stack.isEmpty()) {
+            int currentVertex = stack.pop();
+
+            if (isVisited[currentVertex] <= 0) {
+                isVisited[currentVertex] = count++;
+
+                for (int nextVertex : graph.get(currentVertex)) {
+                    if (isVisited[nextVertex] <= 0) {
+                        stack.push(nextVertex);
+                    }
+                }
+            }
+        }
+    }
+}
