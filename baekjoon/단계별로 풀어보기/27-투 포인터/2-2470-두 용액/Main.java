36f268969a069499105522dfd421db8931766b62
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-20 14:40:22 +0900

    Add 백준 27단계 투 포인터 2-2470-두 용액

---
+++ baekjoon/단계별로 풀어보기/27-투 포인터/2-2470-두 용액/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] array = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        int index1 = 0;
+        int index2 = 0;
+        int start = 0;
+        int end = N - 1;
+        int min = Integer.MAX_VALUE;
+
+        while (start < end) {
+            int sum = array[start] + array[end];
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                index1 = start;
+                index2 = end;
+            }
+
+            if (sum < 0) {
+                start++;
+            } else if (sum > 0) {
+                end--;
+            } else {
+                break;
+            }
+        }
+
+        bw.write(array[index1] + " " + array[index2]);
+
+        bw.flush();
+        bw.close();
+    }
+}
