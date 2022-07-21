93f4a74036ef2891253cca124e079f0f899b5b59
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-21 16:18:13 +0900

    Add study\01-Data Structure 2\03-11286-절댓값 힙

---
+++ study/01-Data Structure 2/03-11286-절댓값 힙/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            if (Math.abs(o1) < Math.abs(o2)) {
+                return -1;
+            } else if (Math.abs(o1) > Math.abs(o2)) {
+                return 1;
+            } else {
+                return o1.compareTo(o2);
+            }
+        });
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (priorityQueue.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(priorityQueue.poll()).append('\n');
+                }
+            } else {
+                priorityQueue.add(x);
+            }
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+}
