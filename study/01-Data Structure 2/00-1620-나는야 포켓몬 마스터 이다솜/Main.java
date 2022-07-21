4e49d0c1a32ba8a9e85274addeb832b8ffca57f9
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-21 16:13:28 +0900

    Add study\01-Data Structure 2\00-1620-나는야 포켓몬 마스터 이다솜

---
+++ study/01-Data Structure 2/00-1620-나는야 포켓몬 마스터 이다솜/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            hashMap.put(Integer.toString(i), str);
+            hashMap.put(str, Integer.toString(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(hashMap.get(br.readLine())).append('\n');
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
