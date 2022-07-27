d5c54577d62ee6c0334f99b1ee028937e43df899
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-27 11:14:21 +0900

    Add memo\CreateTree

---
+++ memo/CreateTree/src/Main.java
@@ -0,0 +1,6 @@
+// reference : https://www.baeldung.com/java-binary-tree
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ memo/CreateTree/src/Tree.java
@@ -0,0 +1,24 @@
+class Tree {
+
+    public Node root;
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.getValue()) {
+            current.setLeft(addRecursive(current.getLeft(), value));
+        } else if (value > current.getValue()) {
+            current.setRight(addRecursive(current.getRight(), value));
+        } else {
+            return current;
+        }
+
+        return current;
+    }
+}
