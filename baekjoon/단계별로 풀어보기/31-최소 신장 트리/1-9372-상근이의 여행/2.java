257fb83c7ab2108c5fdeb943636ba0af15228927
49EHyeon42 <49EHyeon42@gmail.com>
2022-07-31 18:45:34 +0900

    Add 백준 31단계 최소 신장 트리 1-9372-상근이의 여행 2번 풀이

---
+++ baekjoon/단계별로 풀어보기/31-최소 신장 트리/1-9372-상근이의 여행/2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 최소 신장 트리의 특징을 사용
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < M; j++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+            }
+
+            sb.append(N - 1).append('\n');
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+    }
+}
