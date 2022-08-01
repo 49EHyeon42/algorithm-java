9e630bd43bcec72684c779f31dec8c2d9d5e1f55
49EHyeon42 <49EHyeon42@gmail.com>
2022-08-01 12:37:52 +0900

    Add 백준 30단계 유니온 파인드 4-20040-사이클 게임

---
+++ baekjoon/단계별로 풀어보기/30-유니온 파인드/4-20040-사이클 게임/Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (isUnion(x, y)) {
+                answer = i;
+                break;
+            } else {
+                merge(x, y);
+            }
+        }
+        
+        bw.write(Integer.toString(answer));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    private static boolean isUnion(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    private static int find(int index) {
+        return (parent[index] == index) ? index : (parent[index] = find(parent[index]));
+    }
+}
