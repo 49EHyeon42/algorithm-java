23d00d965b62a1e0a91443bb99c5d4a2c3198568
49EHyeon42 <49EHyeon42@gmail.com>
2022-08-23 22:09:46 +0900

    Add Java\Level 2\H-Index

---
+++ Java/Level 2/H-Index/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // test 1
+        int[] citations1 = {3, 0, 6, 1, 5};
+        int result1 = 3;
+
+        System.out.println("test case 1 = " + (solution.solution(citations1) == result1));
+    }
+}
