872a2350a3ac36380362544b1779f4a9d3a22912
49EHyeon42 <49EHyeon42@gmail.com>
2022-08-23 17:20:51 +0900

    Add baekjoon\전체 문제\1476-날짜 계산

---
+++ baekjoon/전체 문제/1476-날짜 계산/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int year = 1;
+        while ((year - E) % 15 != 0 || (year - S) % 28 != 0 || (year - M) % 19 != 0) {
+            year++;
+        }
+
+        bw.write(Long.toString(year));
+        bw.flush();
+    }
+}
