d295d2bfdfc4eeeb6130b4fafa15600764cec2a6
49EHyeon42 <49EHyeon42@gmail.com>
2022-09-05 09:45:56 +0900

    Rename memo\최소 신장 트리 (Minimum spanning tree)

---
+++ memo/최소 신장 트리 (Minimum spanning tree)/프림 알고리즘 (Prim's algorithm)/src/Vertex.java
@@ -0,0 +1,18 @@
+public class Vertex {
+
+    private final int vertex;
+    private final int weight;
+
+    public Vertex(int vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+
+    public int getVertex() {
+        return vertex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
