c912cb699331d6ff07d7870029c0d8eb7e347e1d
49EHyeon42 <49EHyeon42@gmail.com>
2022-09-17 12:29:26 +0900

    Refactor baekjoon\전체 문제\1202-보석 도둑

--- baekjoon/전체 문제/1202-보석 도둑/Main.java
+++ baekjoon/전체 문제/1202-보석 도둑/Main.java
@@ -3,11 +3,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// 시간초과
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -18,10 +19,8 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Jewelry> jewels = new PriorityQueue<>(
-            Comparator.comparingInt(Jewelry::getWeight));
-
-        PriorityQueue<Integer> bags = new PriorityQueue<>();
+        ArrayList<Jewelry> jewels = new ArrayList<>();
+        int[] bags = new int[K];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -32,21 +31,29 @@ public class Main {
         }
 
         for (int i = 0; i < K; i++) {
-            int C = Integer.parseInt(br.readLine());
-
-            bags.add(C);
+            bags[i] = Integer.parseInt(br.readLine());
         }
+
+        jewels.sort((o1, o2) -> {
+            if (o1.getWeight() == o2.getWeight()) {
+                return o2.getPrice() - o1.getPrice();
+            }
+            return o1.getWeight() - o2.getWeight();
+        });
+
+        Arrays.sort(bags);
 
         long maxPrice = 0;
 
-        while (!bags.isEmpty()) {
-            int currentBagWeight = bags.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-            while (!jewels.isEmpty()) {
-                if (currentBagWeight >= jewels.peek().getWeight()) {
-                    maxPrice += jewels.poll().getPrice();
-                    break;
-                }
+        for (int i = 0, index = 0; i < K; i++) {
+            while (index < N && jewels.get(index).getWeight() <= bags[i]) {
+                pq.offer(jewels.get(index++).getPrice());
+            }
+
+            if (!pq.isEmpty()) {
+                maxPrice += pq.poll();
             }
         }
 
