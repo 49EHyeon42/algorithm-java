3a9f42507c0db435c4547da3301611d33fd76ed3
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-23 15:28:37 +0900

    Refactor LeetCode\LeetCode 75\Level 1\04-392-Is Subsequence Day 2

--- LeetCode/LeetCode 75/Level 1/04-392-Is Subsequence/Solution.java
+++ LeetCode/LeetCode 75/Level 1/04-392-Is Subsequence/Solution.java
@@ -1,22 +1,14 @@
 class Solution {
 
     public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) {
-            return true;
-        }
-        if (s.length() > t.length()) {
-            return false;
-        }
-
         int indexS = 0;
-        for (int indexT = 0; indexT < t.length(); indexT++) {
+
+        for (int indexT = 0; indexT < t.length() && indexS < s.length(); indexT++) {
             if (s.charAt(indexS) == t.charAt(indexT)) {
-                if (++indexS == s.length()) {
-                    return true;
-                }
+                indexS++;
             }
         }
-            
+
         return indexS == s.length();
     }
 }
