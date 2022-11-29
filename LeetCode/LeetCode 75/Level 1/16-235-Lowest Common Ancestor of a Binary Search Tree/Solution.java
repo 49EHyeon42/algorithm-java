595663e7b6ec2c6992a0654fb26b2a22339a7bda
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-29 19:17:47 +0900

    add: LeetCode\LeetCode 75\Level 1\16-235-Lowest Common Ancestor of a Binary Search Tree 풀이 2

--- LeetCode/LeetCode 75/Level 1/16-235-Lowest Common Ancestor of a Binary Search Tree/Solution.java
+++ LeetCode/LeetCode 75/Level 1/16-235-Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -18,4 +18,11 @@ class Solution {
         }
         return root;
     }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0) {
+            root = (p.val < root.val) ? root.left : root.right;
+        }
+        return root;
+    }
 }
