25c20a930df7a300762247c92a371b2637cfd3d2
49ehyeon42 <49ehyeon42@gmail.com>
2022-12-01 13:09:58 +0900

    refactor: LeetCode\LeetCode 75\Level 1\19-509-Fibonacci Number

--- LeetCode/LeetCode 75/Level 1/19-509-Fibonacci Number/Solution.java
+++ LeetCode/LeetCode 75/Level 1/19-509-Fibonacci Number/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
 
+    // Time complexity = O(N), Space complexity = O(N)
     public int fib(int n) {
         if (n <= 1) {
             return n;
@@ -13,5 +14,23 @@ class Solution {
         }
 
         return dp[n];
+    }
+
+    // Time complexity = O(N), Space complexity = O(1)
+    public int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int temp1 = 0;
+        int temp2 = 1;
+
+        while (n-- > 1) {
+            int sum = temp1 + temp2;
+            temp1 = temp2;
+            temp2 = sum;
+        }
+
+        return temp2;
     }
 }
