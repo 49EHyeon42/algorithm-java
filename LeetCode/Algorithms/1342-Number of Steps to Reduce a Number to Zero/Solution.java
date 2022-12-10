1f327a15e4af3e419291562cfe81db5506136b63
49ehyeon42 <49ehyeon42@gmail.com>
2022-12-10 16:13:11 +0900

    add: LeetCode\Algorithms\1342-Number of Steps to Reduce a Number to Zero, 비트 연산 풀이 추가

--- LeetCode/Algorithms/1342-Number of Steps to Reduce a Number to Zero/Solution.java
+++ LeetCode/Algorithms/1342-Number of Steps to Reduce a Number to Zero/Solution.java
@@ -16,4 +16,10 @@ class Solution {
     public int numberOfSteps2(int num) {
         return (num == 0) ? 0 : 1 + numberOfSteps((num % 2 == 0) ? num / 2 : num - 1);
     }
+
+    // Bitwise operation
+    // link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/solutions/907422/java-0ms-1-liner-avoid-recursion-iteration-with-integer-library/
+    public int numberOfSteps(int num) {
+        return Integer.toBinaryString(num).length() - 1 + Integer.bitCount(num);
+    }
 }
