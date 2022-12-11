fc3e1914555bf28a90911a6d1ade1d56dba07062
49ehyeon42 <49ehyeon42@gmail.com>
2022-12-11 15:44:33 +0900

    add: LeetCode\Algorithms\0009-Palindrome Number, 다른 풀이 추가

--- LeetCode/Algorithms/0009-Palindrome Number/Solution.java
+++ LeetCode/Algorithms/0009-Palindrome Number/Solution.java
@@ -10,4 +10,50 @@ class Solution {
         }
         return true;
     }
+
+        public boolean isPalindrome2(int x) {
+        int original = x;
+        int rev = 0;
+
+        while (x > 0) {
+            rev = x % 10 + rev * 10;
+            x = x / 10;
+        }
+
+        return rev == original;
+    }
+
+    public boolean isPalindrome3(int x) {
+        //optimizations
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+        if (x % 10 == 0) {
+            return false;
+        }
+        if (x < 100 && x % 11 == 0) {
+            return true;
+        }
+        if (x < 1000 && ((x / 100) * 10 + x % 10) % 11 == 0) {
+            return true;
+        }
+
+        //actual logic
+        int v = x % 10;
+        x = x / 10;
+
+        while (x - v > 0) {
+            v = v * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (v > x) {
+            v /= 10;
+        }
+
+        return v == x;
+    }
 }
