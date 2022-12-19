43af35ddb82cf6cbefa778dd9991d87b8f68ed16
49ehyeon42 <49ehyeon42@gmail.com>
2022-12-19 09:57:19 +0900

    add: LeetCode\Algorithms\0713-Subarray Product Less Than K, 로그 이진 탐색 풀이 추가

--- LeetCode/Algorithms/0713-Subarray Product Less Than K/Solution.java
+++ LeetCode/Algorithms/0713-Subarray Product Less Than K/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
 
+    // 슬라이딩 윈도우
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if (k == 0) {
             return 0;
@@ -19,5 +20,39 @@ class Solution {
         }
 
         return count;
+    }
+
+    // 로그 이진 탐색
+    public int numSubarrayProductLessThanK2(int[] nums, int k) {
+        if (k == 0) {
+            return 0;
+        }
+
+        double logk = Math.log(k);
+        double[] prefix = new double[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i + 1] = prefix[i] + Math.log(nums[i]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < prefix.length; i++) {
+            int lo = i + 1, hi = prefix.length;
+
+            while (lo < hi) {
+                int mi = lo + (hi - lo) / 2;
+
+                if (prefix[mi] < prefix[i] + logk - 1e-9) {
+                    lo = mi + 1;
+                } else {
+                    hi = mi;
+                }
+            }
+
+            ans += lo - i - 1;
+        }
+
+        return ans;
     }
 }
