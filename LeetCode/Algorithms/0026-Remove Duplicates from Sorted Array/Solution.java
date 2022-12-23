a4554532f6b2393a92763727a2465fe16ad4c137
49EHyeon42 <49ehyeon42@gmail.com>
2022-12-23 15:51:29 +0900

    add: LeetCode\Algorithms\0026-Remove Duplicates from Sorted Array, 2번 풀이

--- LeetCode/Algorithms/0026-Remove Duplicates from Sorted Array/Solution.java
+++ LeetCode/Algorithms/0026-Remove Duplicates from Sorted Array/Solution.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
+    // slow
     public int removeDuplicates(int[] nums) {
         List<Integer> list = Arrays.stream(nums).boxed().distinct().sorted(Integer::compareTo).collect(Collectors.toList());
 
@@ -12,5 +13,18 @@ public class Solution {
         }
 
         return list.size();
+    }
+
+    // fast
+    public int removeDuplicates2(int[] nums) {
+        int i = 0;
+
+        for (int num : nums) {
+            if (i == 0 || num > nums[i - 1]) {
+                nums[i++] = num;
+            }
+        }
+
+        return i;
     }
 }
