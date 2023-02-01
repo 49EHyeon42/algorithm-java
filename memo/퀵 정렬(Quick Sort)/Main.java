2322830f82cb2ff7f77cb041e254b6d9b54bd735
49EHyeon42 <49ehyeon42@gmail.com>
2023-02-01 18:15:37 +0900

    add: memo\퀵 정렬(Quick Sort) 오른쪽 피벗, 중간 피벗 정렬 방식 추가

--- memo/퀵 정렬(Quick Sort)/Main.java
+++ memo/퀵 정렬(Quick Sort)/Main.java
@@ -1,47 +1,133 @@
+// reference : https://st-lab.tistory.com/250
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[]{-1, 12, 11, 13, 5, 6, 7};
+        int[] array = new int[]{2, -5, 6, 7, 1, 5, 2};
 
         System.out.print("before sorting = ");
         printArray(array);
 
-        quickSort(array, 0, array.length - 1);
+        // leftSort(array, 0, array.length - 1);
+        // rightSort(array, 0, array.length - 1);
+        // middleSort(array, 0, array.length - 1);
 
         System.out.print("after sorting = ");
         printArray(array);
     }
 
-    // 왼쪽 피벗을 활용한 퀵 정렬
-    private static void quickSort(int[] array, int low, int high) {
-        if (low < high) {
-            int i = low + 1, j = high;
+    // 왼쪽 피벗 퀵 정렬
+    private static void leftSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
 
-            while (i <= j) {
-                while (array[i] <= array[low]) {
-                    i++;
-                }
+        int pivot = leftPartition(array, low, high);
 
-                while (array[j] > array[low]) {
-                    j--;
-                }
+        leftSort(array, low, pivot - 1);
+        leftSort(array, pivot + 1, high);
+    }
 
-                if (i < j) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
+    private static int leftPartition(int[] array, int left, int right) {
+        int low = left;
+        int high = right;
+        int pivot = array[left];
+
+        while (low < high) {
+            while (array[high] > pivot && low < high) {
+                high--;
             }
 
-            int temp = array[low];
-            array[low] = array[j];
-            array[j] = temp;
+            while (array[low] <= pivot && low < high) {
+                low++;
+            }
 
-            quickSort(array, low, j - 1);
-            quickSort(array, j + 1, high);
+            int swap = array[low];
+            array[low] = array[high];
+            array[high] = swap;
+        }
+
+        int swap = array[left];
+        array[left] = array[low];
+        array[low] = swap;
+
+        return low;
+    }
+
+    // 오른쪽 피벗 퀵 정렬
+    private static void rightSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int pivot = rightPartition(array, low, high);
+
+        rightSort(array, low, pivot - 1);
+        rightSort(array, pivot + 1, high);
+    }
+
+    private static int rightPartition(int[] array, int left, int right) {
+        int low = left;
+        int high = right;
+        int pivot = array[right];
+
+        while (low < high) {
+            while (array[low] < pivot && low < high) {
+                low++;
+            }
+
+            while (array[high] >= pivot && low < high) {
+                high--;
+            }
+
+            int swap = array[low];
+            array[low] = array[high];
+            array[high] = swap;
+        }
+
+        int swap = array[right];
+        array[right] = array[high];
+        array[high] = swap;
+
+        return high;
+    }
+
+    // 중간 피벗 퀵 정렬
+    private static void middleSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int pivot = middlePartition(array, low, high);
+
+        middleSort(array, low, pivot - 1);
+        middleSort(array, pivot + 1, high);
+    }
+
+    private static int middlePartition(int[] array, int left, int right) {
+        int low = left - 1;
+        int high = right + 1;
+        int pivot = array[(left + right) / 2];
+
+        while (true) {
+            do {
+                low++;
+            } while (array[low] < pivot);
+
+            do {
+                high--;
+            } while (array[high] > pivot && low <= high);
+
+            if (low >= high) {
+                return high;
+            }
+
+            int swap = array[low];
+            array[low] = array[high];
+            array[high] = swap;
         }
     }
 
+    // 배열 출력 확인용
     private static void printArray(int[] array) {
         for (int i : array) {
             System.out.print(i + " ");
