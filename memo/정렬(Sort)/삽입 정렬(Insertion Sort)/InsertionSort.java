805a623d82b0ce3a35b8124948c9ad97220f022c
49EHyeon42 <49ehyeon42@gmail.com>
2023-02-06 17:50:32 +0900

    add: 삽입 정렬 예제

---
+++ memo/정렬(Sort)/삽입 정렬(Insertion Sort)/InsertionSort.java
@@ -0,0 +1,38 @@
+public class InsertionSort {
+
+    public void naturalInsertionSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 1; i < length; i++) {
+            int key = array[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+
+    public void reverseInsertionSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 1; i < length; i++) {
+            int key = array[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && array[j] < key) {
+                array[j + 1] = array[j];
+
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+}
