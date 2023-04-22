243937b5dce110cef2bff772649b96a4bd1d9962
49EHyeon42 <49ehyeon42@gmail.com>
2023-04-22 15:39:12 +0900

    add: 프로그래머스(programmers)\연습문제\최솟값 만들기

---
+++ 프로그래머스(programmers)/연습문제/최솟값 만들기/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+
+        for (int i = 0, j = B.length - 1; i < A.length; i++, j--) {
+            answer += A[i] * B[j];
+        }
+
+        return answer;
+    }
+}
