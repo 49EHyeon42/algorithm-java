0f73dd8ea19d332885471ea306661bce73663c98
49EHyeon42 <49ehyeon42@gmail.com>
2023-05-04 21:09:10 +0900

    백준 템플릿 추가

---
+++ baekjoon/템플릿/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Solution {
+
+    public void solution() {
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Solution solution = new Solution();
+
+        bw.write();
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
