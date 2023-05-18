fd6cc119c3b4738bc8c90e7d715e2e9aee172291
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-05-18 17:47:11 +0900

    refactor: baekjoon\전체 문제\15312-이름 궁합

--- baekjoon/전체 문제/15312-이름 궁합/Main.java
+++ baekjoon/전체 문제/15312-이름 궁합/Main.java
@@ -18,25 +18,20 @@ public class Main {
 
         int maxLength = hisName.length() * 2;
 
-        int[][] dp = new int[maxLength - 1][maxLength];
-
-//        for (int i = 0; i < maxLength; i++) {
-//            dp[0][i] = alphabet[((i & 1) == 0 ?
-//                    hisName.charAt(i >> 1) : herName.charAt(i >> 1)) - 'A'];
-//        }
+        int[] dp = new int[maxLength];
 
         for (int i = 0; i < hisName.length(); i++) {
-            dp[0][i << 1] = alphabet[hisName.charAt(i) - 'A'];
-            dp[0][i << 1 | 1] = alphabet[herName.charAt(i) - 'A'];
+            dp[i * 2] = alphabet[hisName.charAt(i) - 'A'];
+            dp[i * 2 + 1] = alphabet[herName.charAt(i) - 'A'];
         }
 
         for (int i = 1; i < maxLength - 1; i++) {
             for (int j = 0; j < maxLength - i; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j + 1]) % 10;
+                dp[j] = (dp[j] + dp[j + 1]) % 10;
             }
         }
 
-        bw.write(String.format("%d%d", dp[maxLength - 2][0], dp[maxLength - 2][1]));
+        bw.write(String.format("%d%d", dp[0], dp[1]));
         bw.flush();
 
         br.close();
