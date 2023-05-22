dceb1d3425940ab929c4314b28851e79c35f013e
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-05-22 12:17:08 +0900

    add: baekjoon\전체 문제\8394-악수, DP

---
+++ baekjoon/전체 문제/8394-악수/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    private static int dp(int n) {
+        if (n < 4) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString(dp(n)));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
