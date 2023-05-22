15790650f4598a482b670056f053a3245620ea5f
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-05-22 12:58:11 +0900

    add: baekjoon\전체 문제\17626-Four Squares, DP

---
+++ baekjoon/전체 문제/17626-Four Squares/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    private static int dp(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+
+            dp[i] = min + 1;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString(dp(n)));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
