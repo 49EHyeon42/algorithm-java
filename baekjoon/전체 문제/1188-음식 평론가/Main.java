2b094469526ded19eda625cbfb7fe250dbd3b052
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-06-27 20:20:27 +0900

    add: baekjoon\전체 문제\1188-음식 평론가, 정수론

---
+++ baekjoon/전체 문제/1188-음식 평론가/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // 증명 : https://www.acmicpc.net/board/view/15979
+
+    private static int gcd(int a, int b) {
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b != 0) {
+            int i = a % b;
+            a = b;
+            b = i;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString(M - gcd(N, M)));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
