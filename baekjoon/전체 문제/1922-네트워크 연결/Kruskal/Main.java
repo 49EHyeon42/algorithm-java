a2ec64674df72dbb5519d66c339db5368a5b24be
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-05 12:25:28 +0900

    add: baekjoon\전체 문제\1922-네트워크 연결, MST(크루스칼, 프림 (속도 차이 없음))

---
+++ baekjoon/전체 문제/1922-네트워크 연결/Kruskal/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final ArrayList<Edge> edges = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int vertexNumber = Integer.parseInt(br.readLine());
+        int edgeNumber = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < edgeNumber; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int vertex1 = Integer.parseInt(st.nextToken());
+            int vertex2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(vertex1, vertex2, weight));
+        }
+
+
+        edges.sort(Comparator.comparingInt(edge -> edge.weight));
+
+        Kruskal kruskal = new Kruskal(vertexNumber);
+
+        int totalWeight = 0;
+
+        for (Edge edge : edges) {
+            if (!kruskal.isUnion(edge.vertex1, edge.vertex2)) {
+                kruskal.merge(edge.vertex1, edge.vertex2);
+                totalWeight += edge.weight;
+            }
+        }
+
+        bw.write(Integer.toString(totalWeight));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static class Edge {
+
+        private final int vertex1;
+        private final int vertex2;
+        private final int weight;
+
+        Edge(int vertex1, int vertex2, int weight) {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.weight = weight;
+        }
+    }
+
+    private static class Kruskal {
+
+        // private
+        final int[] parent;
+
+        Kruskal(int vertexCount) {
+            parent = new int[vertexCount + 1];
+            for (int i = 1; i <= vertexCount; i++) {
+                parent[i] = i;
+            }
+        }
+
+        // public
+        void merge(int x, int y) {
+            int tempX = find(x);
+            int tempY = find(y);
+            if (tempX != tempY) {
+                parent[tempY] = tempX;
+            }
+        }
+
+        // public
+        boolean isUnion(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        // private
+        int find(int index) {
+            return (parent[index] == index) ? index : (parent[index] = find(parent[index]));
+        }
+    }
+}
---
+++ baekjoon/전체 문제/1922-네트워크 연결/Prim/Main.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static List<List<Vertex>> graph;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int vertexNumber = Integer.parseInt(br.readLine());
+        int edgeNumber = Integer.parseInt(br.readLine());
+
+        // init
+        graph = new ArrayList<>();
+
+        for (int i = 0; i <= vertexNumber; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        visited = new boolean[vertexNumber + 1];
+
+        for (int i = 0; i < edgeNumber; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int vertex1 = Integer.parseInt(st.nextToken());
+            int vertex2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph.get(vertex1).add(new Vertex(vertex2, weight));
+            graph.get(vertex2).add(new Vertex(vertex1, weight));
+        }
+
+        bw.write(Integer.toString(prim()));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int prim() {
+        int minimumWeight = 0;
+
+        // pq = priorityQueue
+        PriorityQueue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> vertex.weight));
+
+        // start vertex = 1
+        pq.offer(new Vertex(1, 0));
+
+        while (!pq.isEmpty()) {
+            Vertex currentVertex = pq.poll();
+
+            if (visited[currentVertex.number]) {
+                continue;
+            }
+
+            minimumWeight += currentVertex.weight;
+
+            visited[currentVertex.number] = true;
+
+            for (Vertex nextVertex : graph.get(currentVertex.number)) {
+                if (visited[nextVertex.number]) {
+                    continue;
+                }
+
+                pq.offer(nextVertex);
+            }
+        }
+
+        return minimumWeight;
+    }
+
+    private static class Vertex {
+
+        private final int number;
+        private final int weight;
+
+        Vertex(int number, int weight) {
+            this.number = number;
+            this.weight = weight;
+        }
+    }
+}
