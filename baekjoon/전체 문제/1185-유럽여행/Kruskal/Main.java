12927632b5b017489e91a281a30d8b1fcb39da79
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-20 15:06:56 +0900

    add: baekjoon\전체 문제\1185-유럽여행, MST(크루스칼), MST 응용 문제

---
+++ baekjoon/전체 문제/1185-유럽여행/Kruskal/Main.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int vertexNumber = Integer.parseInt(st.nextToken());
+        int edgeNumber = Integer.parseInt(st.nextToken());
+
+        int[] vertexWeight = new int[vertexNumber + 1];
+
+        int minimumVertexWeight = 1001;
+
+        for (int i = 1; i <= vertexNumber; i++) {
+            vertexWeight[i] = Integer.parseInt(br.readLine());
+
+            minimumVertexWeight = Math.min(minimumVertexWeight, vertexWeight[i]);
+        }
+
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < edgeNumber; i++) {
+            st = new StringTokenizer(br.readLine());
+            int vertex1 = Integer.parseInt(st.nextToken());
+            int vertex2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            // INFO
+            // 왕복 가중치 = A to B -> to B : B 가중치 + edge 가중치, to A : A 가중치 + edge 가중치
+            //            = A 가중치 + B 가중치 + 2 * edge 가중치
+            weight = vertexWeight[vertex1] + vertexWeight[vertex2] + 2 * weight;
+
+            edges.add(new Edge(vertex1, vertex2, weight));
+        }
+
+        edges.sort(Comparator.comparingInt(edge -> edge.weight));
+
+        // Kruskal
+
+        int minimumEdgeWeight = 0;
+
+        Kruskal kruskal = new Kruskal(vertexNumber);
+
+        for (Edge edge : edges) {
+            if (kruskal.isUnion(edge.vertex1, edge.vertex2)) {
+                continue;
+            }
+
+            kruskal.merge(edge.vertex1, edge.vertex2);
+
+            minimumEdgeWeight += edge.weight;
+        }
+
+        bw.write(Integer.toString(minimumVertexWeight + minimumEdgeWeight));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static class Edge {
+
+        private final int vertex1;
+        private final int vertex2;
+        private final int weight;
+
+
+        Edge(int vertex1, int vertex2, int weight) {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.weight = weight;
+        }
+    }
+
+    private static class Kruskal {
+
+        // private
+        final int[] parent;
+
+        Kruskal(int vertexCount) {
+            parent = new int[vertexCount + 1];
+            for (int i = 1; i <= vertexCount; i++) {
+                parent[i] = i;
+            }
+        }
+
+        // public
+        void merge(int x, int y) {
+            int tempX = find(x);
+            int tempY = find(y);
+            if (tempX != tempY) {
+                parent[tempY] = tempX;
+            }
+        }
+
+        // public
+        boolean isUnion(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        // private
+        int find(int index) {
+            return (parent[index] == index) ? index : (parent[index] = find(parent[index]));
+        }
+    }
+}
