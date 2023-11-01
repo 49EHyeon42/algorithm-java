abe3f6ca220eec6fff400f6d6dcfeaf22f815f92
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-11-01 13:01:50 +0900

    add: baekjoon\전체 문제\7044-Bad Cowtractors, MST(크루스칼, 프림)

---
+++ baekjoon/전체 문제/7044-Bad Cowtractors/Prim/Main.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<List<Vertex>> graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int vertex1 = Integer.parseInt(st.nextToken());
+            int vertex2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph.get(vertex1).add(new Vertex(vertex2, weight));
+            graph.get(vertex2).add(new Vertex(vertex1, weight));
+        }
+
+        int totalWeight = 0;
+
+        PriorityQueue<Vertex> pq = new PriorityQueue<>((vertex1, vertex2) -> vertex2.weight - vertex1.weight);
+        pq.offer(new Vertex(1, 0));
+
+        boolean[] visited = new boolean[N + 1];
+
+        while (!pq.isEmpty()) {
+            Vertex currentVertex = pq.poll();
+
+            if (visited[currentVertex.number]) {
+                continue;
+            }
+
+            visited[currentVertex.number] = true;
+
+            totalWeight += currentVertex.weight;
+
+            for (Vertex nextVertex : graph.get(currentVertex.number)) {
+                if (visited[nextVertex.number]) {
+                    continue;
+                }
+
+                pq.offer(nextVertex);
+            }
+        }
+
+        bw.write(Integer.toString(isUnionAll(visited) ? totalWeight : -1));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean isUnionAll(boolean[] visited) {
+        for (int i = 1; i < visited.length; i++) {
+            if (!visited[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static class Vertex {
+
+        final int number;
+        final int weight;
+
+        Vertex(int number, int weight) {
+            this.number = number;
+            this.weight = weight;
+        }
+    }
+}
