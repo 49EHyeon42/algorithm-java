b3d7a4cd591e74882153406559758e267457ff7e
49EHyeon42 <49ehyeon42@gmail.com>
2024-09-19 09:40:59 +0900

    add: baekjoon/전체 문제/5972-택배 배송, dijkstra, 재풀이

--- baekjoon/전체 문제/5972-택배 배송/Main.java
+++ baekjoon/전체 문제/5972-택배 배송/Main.java
@@ -1,7 +1,16 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
+
+    private static final Map<Integer, Set<Vertex>> graph = new HashMap<>();
+    private static final PriorityQueue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> vertex.distance));
+
+    private static int[] minimumDistances;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,56 +20,65 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<List<Vertex>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+        minimumDistances = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph.put(i, new HashSet<>());
+
+            minimumDistances[i] = Integer.MAX_VALUE;
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int vertex1 = Integer.parseInt(st.nextToken());
             int vertex2 = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int distance = Integer.parseInt(st.nextToken());
 
-            graph.get(vertex1).add(new Vertex(vertex2, weight));
-            graph.get(vertex2).add(new Vertex(vertex1, weight));
+            graph.get(vertex1).add(new Vertex(vertex2, distance));
+            graph.get(vertex2).add(new Vertex(vertex1, distance));
         }
 
-        int[] dijkstra = new int[N + 1];
-        Arrays.fill(dijkstra, Integer.MAX_VALUE);
+        dijkstra();
 
-        dijkstra[1] = 0;
-
-        PriorityQueue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> vertex.weight));
-        pq.offer(new Vertex(1, 0));
-
-        while (!pq.isEmpty()) {
-            Vertex currentVertex = pq.poll();
-
-            for (Vertex nextVertex : graph.get(currentVertex.number)) {
-                if (dijkstra[nextVertex.number] > dijkstra[currentVertex.number] + nextVertex.weight) {
-                    dijkstra[nextVertex.number] = dijkstra[currentVertex.number] + nextVertex.weight;
-
-                    pq.offer(new Vertex(nextVertex.number, dijkstra[nextVertex.number]));
-                }
-            }
-        }
-
-        bw.write(Integer.toString(dijkstra[N]));
+        bw.write(Integer.toString(minimumDistances[N]));
         bw.flush();
 
         br.close();
         bw.close();
     }
 
+    private static void dijkstra() {
+        pq.offer(new Vertex(1, 0));
+        minimumDistances[1] = 0;
+
+        while (!pq.isEmpty()) {
+            Vertex currentVertex = pq.poll();
+
+            if (minimumDistances[currentVertex.number] < currentVertex.distance) {
+                continue;
+            }
+
+            for (Vertex nextVertex : graph.get(currentVertex.number)) {
+                int nextVertexDistance = currentVertex.distance + nextVertex.distance;
+
+                if (minimumDistances[nextVertex.number] <= nextVertexDistance) {
+                    continue;
+                }
+
+                pq.offer(new Vertex(nextVertex.number, nextVertexDistance));
+                minimumDistances[nextVertex.number] = nextVertexDistance;
+            }
+        }
+    }
+
     private static class Vertex {
 
         final int number;
-        final int weight;
+        final int distance;
 
-        Vertex(int number, int weight) {
+        Vertex(int number, int distance) {
             this.number = number;
-            this.weight = weight;
+            this.distance = distance;
         }
     }
 }
