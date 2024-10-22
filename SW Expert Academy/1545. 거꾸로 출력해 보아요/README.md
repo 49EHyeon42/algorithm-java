a5e9579773b6766af5611a2113ce28d6c54c4975
49EHyeon42 <49ehyeon42@gmail.com>
2024-10-22 21:32:01 +0900

    add: SW Expert Academy\1545. 거꾸로 출력해 보아요

---
+++ SW Expert Academy/1545. 거꾸로 출력해 보아요/README.md
@@ -0,0 +1,1 @@
+# [1545. 거꾸로 출력해 보아요](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2gbY0qAAQBBAS0)
---
+++ SW Expert Academy/1545. 거꾸로 출력해 보아요/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        for (int i = N; i >= 0; i--) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+}
