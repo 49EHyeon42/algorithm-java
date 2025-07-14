afdbce9a1d9c9750d4b375da6b645a1bc4ab153f
49EHyeon42 <49ehyeon42@gmail.com>
2025-07-14 21:16:30 +0900

    add: 프로그래머스(programmers)\SQL 고득점 Kit\SELECT\조건에 부합하는 중고거래 댓글 조회하기

---
+++ 프로그래머스(programmers)/SQL 고득점 Kit/SELECT/조건에 부합하는 중고거래 댓글 조회하기/PostgreSQL/2-solution.sql
@@ -0,0 +1,11 @@
+SELECT ugb."TITLE",
+       ugb."BOARD_ID",
+       ugr."REPLY_ID",
+       ugr."WRITER_ID",
+       ugr."CONTENTS",
+       TO_CHAR(ugr."CREATED_DATE", 'YYYY-MM-DD') AS "CREATED_DATE"
+FROM "USED_GOODS_BOARD" ugb
+         JOIN "USED_GOODS_REPLY" ugr ON ugb."BOARD_ID" = ugr."BOARD_ID"
+WHERE ugb."CREATED_DATE" >= DATE '2022-10-01'
+  AND ugb."CREATED_DATE" < DATE '2022-11-01'
+ORDER BY ugr."CREATED_DATE", ugb."TITLE";
