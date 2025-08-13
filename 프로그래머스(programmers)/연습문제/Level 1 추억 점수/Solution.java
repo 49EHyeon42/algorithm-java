4427d7201d57e0f2c33e75bc7c03d5550e721285
49EHyeon42 <49ehyeon42@gmail.com>
2025-08-13 19:06:02 +0900

    add: 프로그래머스(programmers)\연습문제\Level 1 추억 점수

---
+++ 프로그래머스(programmers)/연습문제/Level 1 추억 점수/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int count = 0;
+
+            for (String photoName : photo[i]) {
+                count += map.getOrDefault(photoName, 0);
+            }
+
+            answer[i] = count;
+        }
+
+        return answer;
+    }
+}
